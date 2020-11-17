507952c7d4185b1a2fa0ea6ff7968a547698e7e7
Théo <12476264+theoricher@users.noreply.github.com>
2020-11-17 13:40:25 +0100

    Update DisconnectUsers.ps1

--- PowerShell/DisconnectUsers.ps1
+++ PowerShell/DisconnectUsers.ps1
@@ -13,5 +13,5 @@ Foreach($disconnected_user in $disconnected_users)
 	# Add log
 	Add-Content "$PSScriptRoot\DisconnectedUsers.log" -Value "$(Get-Date -Format 'dd/mm/yyyy hh:mm') | Disconnect user $($disconnected_user.UserName) with session ID : $($disconnected_user.UnifiedSessionId)"
 	# Log off user
-    Invoke-RDUserLogoff -HostServer $((Get-WmiObject win32_computersystem).DNSHostName+"."+(Get-WmiObject win32_computersystem).Domain) $disconnected_user.UnifiedSessionId -Force
+	Invoke-RDUserLogoff -HostServer $((Get-WmiObject win32_computersystem).DNSHostName+"."+(Get-WmiObject win32_computersystem).Domain) $disconnected_user.UnifiedSessionId -Force
 }
